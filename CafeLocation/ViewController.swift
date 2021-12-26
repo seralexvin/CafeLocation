9fa4dfaee91cb3fc7ee575be82e41a9ff8c1763b
Servin Asanov <seralexvin@list.ru>
2021-12-26 22:44:46 +0300

    Custom table view cell

--- CafeLocation/ViewController.swift
+++ CafeLocation/ViewController.swift
@@ -30,11 +30,12 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cafeCell", for: indexPath)
-        cell.textLabel?.text = points[indexPath.row]
-        cell.imageView?.image = UIImage(named: points[indexPath.row])
-        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
-        cell.imageView?.clipsToBounds = true
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cafeCell", for: indexPath) as! CustomTableViewCell
+        
+        cell.nameLabel.text = points[indexPath.row]
+        cell.imageOfPlace.image = UIImage(named: points[indexPath.row])
+        cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+        cell.imageOfPlace?.clipsToBounds = true
         
         return cell
     }
